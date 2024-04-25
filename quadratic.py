0e17311b70a4ce8c089157f02abd44ac754ea85d
papiyon8989 <162721612+papiyon8989@users.noreply.github.com>
2024-04-25 20:32:56 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,15 +1,11 @@
-# Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
-    return "ANSWER HERE"
 
     dis = (b**2)-4*a*c
-    if  dis<0:
+    r1 = (-b + dis**0.5) / (2*a)
+    r2 = (-b - dis**0.5) / (2*a)
 
-        r1 = (-b + dis**0.5) / (2*a)
-        r2 = (-b - dis**0.5) / (2*a)
-
-    return f("{r1}, {r2}")
+    if  dis > 0:
+        return f"({r1}, {r2})"
 
     if dis == 0:
         r1 = -b / (2 * a)
@@ -19,7 +15,7 @@ def roots(a, b, c):
         return "( )"
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+
     return (a*(x**2))+(b*x)+(c)
 
 def to_string(a, b, c):
@@ -43,16 +39,15 @@ def to_string(a, b, c):
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
 
     if a != 0 and b != 0:
-        return f"f'(x) = {2 * a}X + {b}"
+        return f"f'(x) = {2 * a} * X + {b}"
 
     elif a == 0 and b != 0:
         return f"f'(x) = {b}"
 
     elif a != 0 and b == 0:
-        return f"f'(x) = {2 * a}X"
+        return f"f'(x) = {2 * a} * X"
         
     else:
         return "f'(x) = 0"
